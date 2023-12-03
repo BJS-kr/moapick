e37c81cf91f81ea2b919c6f253f2c3510591bbda
bjs <bjs@bjsui-MacBookAir.local>
2023-12-03 23:59:11 +0900

    add query building

--- src/user/user_repository.rs
+++ src/user/user_repository.rs
@@ -1,4 +1,13 @@
-use tokio_postgres::Client;
+use sea_query::{Expr, Iden, PostgresQueryBuilder, Query};
+use tokio_postgres::{Client, Error};
+
+#[derive(Iden)]
+enum User {
+    Table,
+    Id,
+    Name,
+    Email,
+}
 
 pub struct UserRepository {
     client: Client,
@@ -9,8 +18,21 @@ impl UserRepository {
         Self { client }
     }
 
-    pub async fn sign_up(&self, name: String, email: String) -> &str {
-        "signed up"
+    pub async fn sign_up(&self, name: String, email: String) -> Result<(), Error> {
+        let q = Query::select()
+            .from(User::Table)
+            .and_where(Expr::col(User::Email).eq(&email))
+            .to_string(PostgresQueryBuilder);
+
+        let user = self.client.simple_query(&q).await?;
+
+        if user.len() > 0 {
+            // 이미 가입된 유저이므로 에러처리
+        }
+
+        // 가입되지 않은 유저이므로 가입처리
+
+        Ok(())
     }
 
     pub async fn sign_in(&self, email: String) -> &str {
