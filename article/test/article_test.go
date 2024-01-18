9d50cc9ee05d76cca461f412dbb294689e3fc4f3
bjs <bjs@bjsui-MacBookAir.local>
2024-01-18 18:30:43 +0900

    add test

--- article/test/article_test.go
+++ article/test/article_test.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
 	"moapick/db/models"
 	"net/http"
 	"testing"
@@ -13,14 +12,14 @@ import (
 )
 
 func TestArticleController(t *testing.T) {
-	signInResp, err:= http.Post("http://localhost:8080/user/sign-in", "application/json", bytes.NewBuffer([]byte(`{"email": "test@test.com"}`)))
-	
+	signInResp, err := http.Post("http://localhost:8080/user/sign-in", "application/json", bytes.NewBuffer([]byte(`{"email": "test@test.com"}`)))
+
 	if err != nil {
 		t.Error(err.Error())
 	}
 
 	defer signInResp.Body.Close()
-	
+
 	accessTokenBody := make(map[string]string)
 
 	json.NewDecoder(signInResp.Body).Decode(&accessTokenBody)
@@ -32,10 +31,10 @@ func TestArticleController(t *testing.T) {
 
 	var targetArticleId uint
 
-	t.Run("save article", func (t *testing.T) {
+	t.Run("save article", func(t *testing.T) {
 		saveReq, _ := http.NewRequest("POST", "http://localhost:8080/article", bytes.NewBuffer([]byte(`{"link":"https://stackoverflow.com", "title":"개발 핵꿀팁 저장소"}`)))
 		SetHeaders(saveReq, accessToken)
-		
+
 		saveResp, err := http.DefaultClient.Do(saveReq)
 
 		if err != nil {
@@ -44,11 +43,20 @@ func TestArticleController(t *testing.T) {
 
 		defer saveResp.Body.Close()
 
-		PrintReturnBody(saveResp)
-
 		assert.Equal(t, 201, saveResp.StatusCode, "response status code must be 201")
 	})
 	t.Run("get all articles of a user", func(t *testing.T) {
+		// 1. save more articles
+		saveReq_1, _ := http.NewRequest("POST", "http://localhost:8080/article", bytes.NewBuffer([]byte(`{"link":"https://google.com", "title":"검색은 역시 구글"}`)))
+		saveReq_2, _ := http.NewRequest("POST", "http://localhost:8080/article", bytes.NewBuffer([]byte(`{"link":"https://naver.com", "title":"한국인이라면 제발 네이버 씁시다"}`)))
+
+		SetHeaders(saveReq_1, accessToken)
+		SetHeaders(saveReq_2, accessToken)
+
+		http.DefaultClient.Do(saveReq_1)
+		http.DefaultClient.Do(saveReq_2)
+
+		// 첫 번째 테스트에서 1회, 이번 테스트에서 2회 추가로 저장했으니 총 3개의 아티클이 반환되어야 한다.
 		getAllReq, _ := http.NewRequest("GET", "http://localhost:8080/article/all", nil)
 		SetHeaders(getAllReq, accessToken)
 
@@ -65,8 +73,7 @@ func TestArticleController(t *testing.T) {
 		json.NewDecoder(getAllResp.Body).Decode(&getAllRespBody)
 		targetArticleId = getAllRespBody[0].ID
 
-		PrintReturnBody(getAllResp)
-
+		assert.Equal(t, 3, len(getAllRespBody), "response body length must be 3")
 		assert.Equal(t, 200, getAllResp.StatusCode, "response status code must be 200")
 	})
 
@@ -82,19 +89,67 @@ func TestArticleController(t *testing.T) {
 
 		defer getResp.Body.Close()
 
-		PrintReturnBody(getResp)
+		article := models.Article{}
 
+		json.NewDecoder(getResp.Body).Decode(&article)
+
+		assert.Equal(t, targetArticleId, article.ID)
 		assert.Equal(t, 200, getResp.StatusCode, "response status code must be 200")
 	})
-}
 
-func SetHeaders(req *http.Request, accessToken string) {
-	req.Header.Set("Authorization", "Bearer " + accessToken)
-	req.Header.Set("Content-Type", "application/json")
-}
+	t.Run("delete article by id", func(t *testing.T) {
+		delReq, _ := http.NewRequest("DELETE", fmt.Sprintf("http://localhost:8080/article/%d", targetArticleId), nil)
+		SetHeaders(delReq, accessToken)
+		delResp, _ := http.DefaultClient.Do(delReq)
 
-func PrintReturnBody(resp *http.Response) {
-	body, _ := io.ReadAll(resp.Body)
-	fmt.Println(string(body))
-}
+		getAllReq, _ := http.NewRequest("GET", "http://localhost:8080/article/all", nil)
+		SetHeaders(getAllReq, accessToken)
+		getAllResp, _ := http.DefaultClient.Do(getAllReq)
 
+		defer getAllResp.Body.Close()
+
+		articles := make([]models.Article, 0)
+		json.NewDecoder(getAllResp.Body).Decode(&articles)
+
+		// targetId에 해당하는 아티클이 삭제되었으니 새로운 아이디로 교체
+		targetArticleId = articles[0].ID
+
+		assert.Equal(t, 2, len(articles), "all article length must be 2")
+		assert.Equal(t, 200, delResp.StatusCode, "delete response code must be 200")
+	})
+
+	t.Run("update title of saved article", func(t *testing.T) {
+		updateReq, _ := http.NewRequest("PATCH", fmt.Sprintf("http://localhost:8080/article/title/%d", targetArticleId), bytes.NewBuffer([]byte(`{"title": "내 맘대로 정해보는 타이틀 후후"}`)))
+		SetHeaders(updateReq, accessToken)
+		http.DefaultClient.Do(updateReq)
+
+		getReq, _ := http.NewRequest("GET", fmt.Sprintf("http://localhost:8080/article/%d", targetArticleId), nil)
+		SetHeaders(getReq, accessToken)
+		getResp, _ := http.DefaultClient.Do(getReq)
+
+		defer getResp.Body.Close()
+
+		article := models.Article{}
+		json.NewDecoder(getResp.Body).Decode(&article)
+
+		assert.Equal(t, article.Title, "내 맘대로 정해보는 타이틀 후후", "title must be updated as expected")
+	})
+
+	t.Run("delete all article", func(t *testing.T) {
+		deleteAllReq, _ := http.NewRequest("DELETE", "http://localhost:8080/article/all", nil)
+		SetHeaders(deleteAllReq, accessToken)
+		http.DefaultClient.Do(deleteAllReq)
+
+		getAllReq, _ := http.NewRequest("GET", "http://localhost:8080/article/all", nil)
+		SetHeaders(getAllReq, accessToken)
+		getAllResp, _ := http.DefaultClient.Do(getAllReq)
+
+		defer getAllResp.Body.Close()
+
+		articles := make([]models.Article, 0)
+
+		json.NewDecoder(getAllResp.Body).Decode(&articles)
+
+		assert.Equal(t, 0, len(articles), "articles length must be zero because all articles have been deleted")
+	})
+}
